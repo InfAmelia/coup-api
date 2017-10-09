d41ccf4015bcd46a1b08ac597a28b2f1fd0e8c00
Amy <aschultz@invoca.com>
2017-10-08 19:18:37 -0700

    uh... what? challenge -> challenges

--- db/migrate/20171009004758_create_challenges.rb
+++ db/migrate/20171009004758_create_challenges.rb
@@ -1,4 +1,4 @@
-class CreateChallenge < ActiveRecord::Migration[5.1]
+class CreateChallenges < ActiveRecord::Migration[5.1]
   def change
     create_table :challenges do |t|
       t.references :player, foreign_key: true
--- db/migrate/20171009004935_add_name_to_challenge.rb
+++ db/migrate/20171009004935_add_name_to_challenge.rb
@@ -1,4 +1,4 @@
-class AddNameToChallenge < ActiveRecord::Migration[5.1]
+class AddNameToChallenges < ActiveRecord::Migration[5.1]
   def change
     add_column :challenges, :name, :string
   end
