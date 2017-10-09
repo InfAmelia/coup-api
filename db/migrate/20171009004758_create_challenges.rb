076a583c520a9e56369690e942869864cf1f0dd7
Amy <aschultz@invoca.com>
2017-10-08 19:24:09 -0700

    ugh, migrations are being weird, removing the one I wrote by hand and seeing if postgres can play nice now

--- db/migrate/20171009004758_create_challenges.rb
+++ db/migrate/20171009004758_create_challenges.rb
@@ -2,7 +2,7 @@ class CreateChallenges < ActiveRecord::Migration[5.1]
   def change
     create_table :challenges do |t|
       t.references :player, foreign_key: true
-
+      t.string :name
       t.timestamps
     end
   end
--- db/migrate/20171009004935_add_name_to_challenge.rb
+++
@@ -1,5 +0,0 @@
-class AddNameToChallenges < ActiveRecord::Migration[5.1]
-  def change
-    add_column :challenges, :name, :string
-  end
-end
