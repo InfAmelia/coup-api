931b86bcdac039f6fde21bbd86496cddc4121d94
Amy <aschultz@invoca.com>
2017-10-25 19:40:15 -0700

    WIP action cable.. working...?

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,6 +1,10 @@
 class Match < ApplicationRecord
   validates :winner, presence: true
 
+  after_create_commit do
+    MatchCreationEventBroadcastJob.perform_later(self)
+  end
+
   def award_challenge_met(challenge_id)
     ChallengeMet.award(self.winner, challenge_id)
   end
